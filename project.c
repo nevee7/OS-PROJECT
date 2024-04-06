592c291b89afa0d9cab33d1669c1c03350583d2b
nevee7 <161845850+nevee7@users.noreply.github.com>
2024-04-06 15:13:23 +0300

    update - it updates the previous snapshot with the information from the new one, updating it

--- project.c
+++ project.c
@@ -8,8 +8,10 @@
 #include <fcntl.h>
 #include <time.h>
 
+#define MAX_PATH_LENGTH 1000
+
 void listFilesRecursively(const char *basePath, int snapshotFile) {
-    char path[1000];
+    char path[MAX_PATH_LENGTH];
     struct dirent *entry;
     struct stat statbuf;
 
@@ -37,19 +39,11 @@ void listFilesRecursively(const char *basePath, int snapshotFile) {
                 perror("write");
                 exit(EXIT_FAILURE);
             }
-            if (write(STDOUT_FILENO, buffer, len) != len) {
-                perror("write");
-                exit(EXIT_FAILURE);
-            }
-            listFilesRecursively(path, snapshotFile); // Recursively list contents of subdirectories
+            listFilesRecursively(path, snapshotFile);
         } else {
             char buffer[1024];
             int len = snprintf(buffer, sizeof(buffer), "File: %s\n", path);
             if (write(snapshotFile, buffer, len) != len) {
-                perror("write");
-                exit(EXIT_FAILURE);
-            }
-            if (write(STDOUT_FILENO, buffer, len) != len) {
                 perror("write");
                 exit(EXIT_FAILURE);
             }
@@ -62,7 +56,7 @@ void listFilesRecursively(const char *basePath, int snapshotFile) {
 void createSnapshot(const char *basePath) {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    char filename[1000];
+    char filename[MAX_PATH_LENGTH];
     sprintf(filename, "%s/snapshot_%d-%02d-%02d_%02d-%02d-%02d.txt", basePath, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
     int snapshotFile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
@@ -77,14 +71,32 @@ void createSnapshot(const char *basePath) {
     printf("Snapshot created: %s\n", filename);
 }
 
+void updateSnapshot(const char *basePath) {
+    char newSnapshot[MAX_PATH_LENGTH];
+    createSnapshot(basePath);
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    snprintf(newSnapshot, sizeof(newSnapshot), "%s/snapshot_%d-%02d-%02d_%02d-%02d-%02d.txt", basePath, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+    char oldSnapshot[MAX_PATH_LENGTH];
+    snprintf(oldSnapshot, sizeof(oldSnapshot), "%s/previous_snapshot.txt", basePath);
+
+    if (rename(newSnapshot, oldSnapshot) != 0) {
+        perror("rename");
+        exit(EXIT_FAILURE);
+    }
+    printf("Snapshot updated.\n");
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         write(STDERR_FILENO, "Usage: <directory_path>\n", strlen("Usage: <directory_path>\n"));
         exit(EXIT_FAILURE);
     }
-    for( int i = 1 ; i < argc ; i ++){
+
+    for (int i = 1; i < argc; i++) {
         char *path = argv[i];
-        createSnapshot(path);
+        updateSnapshot(path);
     }
 
     return EXIT_SUCCESS;
